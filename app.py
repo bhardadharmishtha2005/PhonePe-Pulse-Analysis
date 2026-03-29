898a2d4f8bd6e1a6604b88956fb6090415accb79
Dharmishtha Bharda <bhardadharmishtha2005@gmail.com>
2026-03-29 16:22:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,6 @@ model = load_prediction_model()
 
 # 4. Simplified Sidebar (Removed Personal Info)
 with st.sidebar:
-    # Change the sidebar image line to this:
-    st.sidebar.image("https://raw.githubusercontent.com/pmndrs/market/main/phonepe.png", width=180)
     st.markdown("### **Project Insight**")
     st.write("This AI-powered tool analyzes historical UPI trends to forecast future transaction volumes across India.")
     st.divider()
